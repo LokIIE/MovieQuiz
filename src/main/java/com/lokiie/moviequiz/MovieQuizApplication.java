3d0c7d3b5bf18c94913dec894b98c15fdbbbd717
LokIIE <srimaga01@gmail.com>
2021-07-08 21:38:53 +0200

    :construction: Init springboot + swagger

---
+++ src/main/java/com/lokiie/moviequiz/MovieQuizApplication.java
@@ -0,0 +1,13 @@
+package com.lokiie.moviequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokiie/moviequiz/MovieQuizApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lokiie.moviequiz;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class MovieQuizApplicationTests {
+
+	@Test
+	void applicationLoads() {
+		assertTrue(true);
+	}
+
+}
